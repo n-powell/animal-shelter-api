42433fc2723431ba52c0780d06eaf25b94d8ff68
Nick Powell <nickpowells@gmail.com>
2017-07-21 10:13:30 -0700

    update seedfile for adoption fee

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,8 @@ class Seed
         age: [1,2,3,4,5,12].sample,
         sex: ["male","female"].sample,
         weight: [5,22,32,42,52,120].sample,
-        available: true
+        available: true,
+        adoption_fee: [75,100,300].sample
       )
       puts "Animal named #{animal.name} age #{animal.age}, is a #{animal.sex} #{animal.animal_type}."
     end
