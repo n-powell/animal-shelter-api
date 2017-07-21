5db37a582bba013f379f5989bc46ec39b1b6b503
Nick Powell <nickpowells@gmail.com>
2017-07-21 14:55:14 -0700

    add some likely needed exceptions.. not all tested as needed yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,20 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid do |exception|
     json_response({ message: exception.message }, :not_found)
   end
+
+  rescue_from ActiveRecord::RecordNotSaved do |exception|
+    json_response({ message: exception.message }, :not_found)
+  end
+
+  rescue_from ActiveRecord::RecordNotDestroyed do |exception|
+    json_response({ message: exception.message }, :not_found)
+  end
+
+  rescue_from ActiveRecord::ActiveRecordError do |exception|
+    json_response({ message: exception.message }, :not_found)
+  end
+
+  rescue_from ActiveRecord::UnknownPrimaryKey do |exception|
+    json_response({ message: exception.message }, :not_found)
+  end
 end
